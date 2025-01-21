fbe5c8f18a23437dfc9e27290f10efd57480d9e7
JOÃO CARVALHO <dev.joaocarvalho@gmail.com>
2025-01-21 08:19:48 -0300

    Create questao-41.py

---
+++ Lista Estrutura de Repetição/questao-41.py
@@ -0,0 +1,22 @@
+'''
+O manual de etiqueta para comportamento na internet dita que é impolido usar letras maiúsculas no texto todo, ou em grande parte do texto, não para destacar uma frase - especialmente se essa frase for realmente importante. Junto com essa regra, também é dito que em aplicativos mensageiros (como uatsapp, telegrama), emails e salas de bate papo, digitar em letras maiúsculas é "COMO SE ESTIVESSE GRITANDO COM O INTERLOCUTOR OU ATÉ MESMO XINGANDO ELE". Como aqui a intenção é chamar atenção, sua tarefa é transformar frases comuns em xingamentos, fazendo com que letras minúsculas se tornem maiúsculas em uma frase.
+
+
+
+Entrada
+A primeira linha apresenta uma string de até 100 caracteres composta por caracteres legíveis e espaço. 
+
+
+
+Saída
+
+Apresente, em uma linha, a frase substituindo todas a letras minúsculas por maiúsculas, conforme os exemplos.
+'''
+# Leitura da entrada
+frase = input()
+
+# Transformação da frase para maiúsculas
+frase_maiuscula = frase.upper()
+
+# Impressão da frase resultante
+print(frase_maiuscula)
